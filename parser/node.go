c6ef37fc6414143025b8aeb1131c04f7b3552226
Markus Braunwarth <17438264+mbraunwarth@users.noreply.github.com>
2022-06-05 10:27:57 +0200

    Added AST node structure

--- parser/node.go
+++ parser/node.go
@@ -1,1 +1,19 @@
 package parser
+
+import "github.com/mbraunwarth/lisp/lexer"
+
+type node struct {
+	typ              nodeType
+	line, col, depth int
+	childs           []*node
+}
+
+func makeNode(tok lexer.Token, depth int, typ nodeType) *node {
+	return &node{
+		typ:    typ,
+		line:   tok.Line,
+		col:    tok.col,
+		depth:  depth,
+		childs: make([]*node, 0),
+	}
+}
